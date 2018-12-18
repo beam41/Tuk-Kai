915bca5869e6142cfafe877ef6ab2c182fbe87f6
Meehoi <beam_baimon@hotmail.com>
2018-12-18 12:51:00 +0700

    Android Build Complete

---
+++ Assets/Scripts/TouchCanvasManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class TouchCanvasManager : MonoBehaviour
+{
+    void Awake()
+    {
+        #if !(UNITY_ANDROID || UNITY_IOS)
+        Destroy(gameObject);
+        #endif
+    }
+}
